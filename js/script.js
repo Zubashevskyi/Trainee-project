951c9046a04bc214072f4b3cf785d879501a2db4
Zubashevskyi <Zubashevskii@gmail.com>
2022-11-14 17:22:26 +0200

    added functional for timer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+
+   // Tabs 
+
+
    const tabs = document.querySelectorAll('.tabheader__item');
    const tabContent = document.querySelectorAll('.tabcontent');
    const tabParent = document.querySelector('.tabheader');
@@ -40,4 +44,64 @@ document.addEventListener('DOMContentLoaded', () => {
          });
       }
    });
+
+
+   // Timer
+
+   const deadline = '2022-12-14';
+
+   function getTimeRemaining(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date());
+      const days = Math.floor(t / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+      const minutes = Math.floor((t / 1000 / 60) % 60);
+      const seconds = Math.floor((t / 1000) % 60);
+      return {
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds,
+      };
+   }
+
+   function getZero(num) {
+      if (num >= 0 && num < 10) {
+         return `0${num}`;
+      } else {
+         return num;
+      }
+   }
+
+   function setClock(selector, endtime) {
+      const timer = document.querySelector(selector);
+      const days = timer.querySelector('#days');
+      const hours = timer.querySelector('#hours');
+      const minutes = timer.querySelector('#minutes');
+      const seconds = timer.querySelector('#seconds');
+      const timeInterval = setInterval(updateClock, 1000);
+
+      updateClock();
+
+      function updateClock() {
+         const t = getTimeRemaining(endtime);
+
+         days.textContent = getZero(t.days);
+         hours.textContent = getZero(t.hours);
+         minutes.textContent = getZero(t.minutes);
+         seconds.textContent = getZero(t.seconds);
+
+
+         if (t.total <= 0) {
+            clearInterval(timeInterval);
+         }
+      }
+
+
+   }
+
+   setClock('.timer', deadline);
 });
+
+
+
