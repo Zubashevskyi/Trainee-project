6c9c48f759b4e5a84e65a4293e07e45501d2a286
Zubashevskyi <Zubashevskii@gmail.com>
2022-11-19 08:30:31 +0200

    added POST request and php fail

--- js/script.js
+++ js/script.js
@@ -244,12 +244,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
    const forms = document.querySelectorAll('form');
 
+   const message = {
+      loading: 'Loading...',
+      success: 'Thanks',
+      failure: 'Ups...',
+   };
+
+   forms.forEach(item => {
+      postData(item);
+   });
+
    function postData(form) {
       form.addEventListener('submit', (e) => {
          e.preventDefault();
 
+         const statusMessage = document.createElement('div');
+         statusMessage.classList.add('status');
+         statusMessage.textContent = message.loading;
+         form.append(statusMessage);
+
          const request = new XMLHttpRequest();
          request.open('POST', 'server.php');
+         request.setRequestHeader('Content-type', 'application/json');
+
+         const formData = new FormData(form);
+
+         const object = {};
+
+         formData.forEach((key, value) => {
+            object[key] = value;
+         });
+
+         const json = JSON.stringify(object);
+
+         request.send(json);
+
+         request.addEventListener('load', () => {
+            if (request.status === 200) {
+               console.log(request.response);
+               statusMessage.textContent = message.success;
+               form.reset();
+               setTimeout(() => {
+                  statusMessage.remove();
+               }, 2000);
+
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         });
       });
    }
 
