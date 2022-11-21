91251652fe2f8ad5386a315233c5e7fbab7732d8
Zubashevskyi <Zubashevskii@gmail.com>
2022-11-21 16:16:14 +0200

    Replaced the slice method with regular expressions

--- js/script.js
+++ js/script.js
@@ -371,11 +371,15 @@ document.addEventListener('DOMContentLoaded', () => {
       dots[slideIndex - 1].style.opacity = 1;
    }
 
+   function deleteNoteDigits(str) {
+      return +str.replace(/\D/g, '');
+   }
+
    prev.addEventListener('click', () => {
       if (offset == 0) {
-         offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+         offset = deleteNoteDigits(width) * (slides.length - 1);
       } else {
-         offset -= +width.slice(0, width.length - 2);
+         offset -= deleteNoteDigits(width);
       }
 
       slidesField.style.transform = `translate(-${offset}px)`;
@@ -394,10 +398,10 @@ document.addEventListener('DOMContentLoaded', () => {
    });
 
    next.addEventListener('click', () => {
-      if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      if (offset == deleteNoteDigits(width) * (slides.length - 1)) {
          offset = 0;
       } else {
-         offset += +width.slice(0, width.length - 2);
+         offset += deleteNoteDigits(width);
       }
 
       slidesField.style.transform = `translate(-${offset}px)`;
@@ -421,7 +425,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
          slideIndex = slideTo;
 
-         offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+         offset = deleteNoteDigits(width) * (slideTo - 1);
 
          slidesField.style.transform = `translate(-${offset}px)`;
 
